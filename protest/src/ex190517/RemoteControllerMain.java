cf7658451fecba289670c15ee1e78a4fe7bb0723
user <user@user-PC>
2019-05-17 16:42:51 +0900

    수정

--- protest/src/ex190517/RemoteControllerMain.java
+++ protest/src/ex190517/RemoteControllerMain.java
@@ -11,7 +11,7 @@ public class RemoteControllerMain {
 		rc.turnOn();
 		rc.turnOff();
 		rc.setVolume(5);
-		//rc에 Audio 객체를 대입해서 Audio의 각 메소드 호출
+		//rc에 Audio 객체를 대 입해서 Audio의 각 메소드 호출
 		rc=new Audio();
 		rc.turnOn();
 		rc.turnOff();
